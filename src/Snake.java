b65989e6de8063bc8bfe2026b8e561251be24b57
vamsi krishna <vamsimkota@gmail.com>
2019-01-21 15:38:45 +0530

    added comments

--- src/Snake.java
+++ src/Snake.java
@@ -6,31 +6,39 @@ import java.io.*;
 import java.util.Arrays; 
 
 public class Snake implements ActionListener {
-    public int [] cellMap = {0,90,91,92,93,94,95,96,97,98,99,89,88,87,86,85,84,83,82,81,80,70,71,72,73,74,75,76,77,78,79,69,
+    //cellMap is used to map player score and image icon array
+    private int [] cellMap = {0,90,91,92,93,94,95,96,97,98,99,89,88,87,86,85,84,83,82,81,80,70,71,72,73,74,75,76,77,78,79,69,
             68,67,66,65,64,63,62,61,60,50,51,52,53,54,55,56,57,58,59,49,48,47,46,45,44,43,42,41,40,30,31,32,33,34,35,
             36,37,38,39,29,28,27,26,25,24,23,22,21,20,10,11,12,13,14,15,16,17,18,19,9,8,6,7,5,4,3,2,1};
 
-    public JFrame mainFrame;
-    public JPanel contentPane;
-    public JPanel gamePanel;
-    public JPanel sidePanel;
-    public JLabel []b;
-    public ImageIcon []ic;
-    public JLabel displayScore, score;
-    public JButton diceButton;
-    public Player p1,p2;
+    private JFrame mainFrame;    // main frame
+    private JPanel contentPane;  // content pane used to hold all the panes
+    private JPanel gamePanel;    // game panel
+    private JPanel sidePanel;    // score board panel
+    private JLabel []b;          // array of label
+    private ImageIcon []ic;      // array of image icons
+    private JLabel displayScore, score; // display score label and score label
+    private JButton diceButton;   // dice Button
+    private Player p1,p2;
 
-    int player1 = 0, player2 = 0;
-    int player1Previous = 0, player2Previous = 0;
-    int diceValue, cellNumber;
+    private int player1 = 0, player2 = 0;
+    private int player1Previous = 0, player2Previous = 0;
+    private int diceValue;
 
-    public Snake(){
+    private Snake(){
+        // initialize player 1
         p1 = new Player("vamsi");
-        mainFrame = new JFrame();
-	    File folder = new File("images/tiles");
-	    File[] fileNames = folder.listFiles();
-    	Arrays.sort(fileNames);
-        gamePanel=new JPanel();
+
+        // game panel with Grid Layout 10 by 10
+        gamePanel =new JPanel(new GridLayout(10,10));
+        gamePanel.setPreferredSize(new Dimension(762, 762));
+
+        // read file names from the folder. it reads entire path
+        File folder = new File("images/tiles");
+        File[] fileNames = folder.listFiles();
+        Arrays.sort(fileNames);
+
+        // create 100 labels and set each label with corresponding image
         b = new JLabel[100];
         ic = new ImageIcon[100];
         for(int i=0;i<100;i++){
@@ -38,22 +46,32 @@ public class Snake implements ActionListener {
     		b[i] = new JLabel(ic[i]);
     		gamePanel.add(b[i]);
         }
-
-        contentPane = new JPanel(new FlowLayout());
-        gamePanel.setLayout(new GridLayout(10,10));  
-        gamePanel.setPreferredSize(new Dimension(762, 762));
-        sidePanel = new JPanel(new GridLayout(3,1));
+        // dice button with action Listener
         diceButton = new JButton(new ImageIcon("images/dice.jpg"));
-        score  = new JLabel("Score");
-        displayScore = new JLabel();
         diceButton.setPreferredSize(new Dimension(126, 126));
+        diceButton.addActionListener(this::actionPerformed);
+
+        score  = new JLabel("Score");
+
+        // show dice value
+        displayScore = new JLabel();
+        displayScore.setForeground(Color.RED);
+        displayScore.setFont(new Font("Serif", Font.PLAIN, 30));
+
+        // side panel
+        sidePanel = new JPanel(new GridLayout(3,1));
+        sidePanel.setPreferredSize(new Dimension(266, 762));
         sidePanel.add(diceButton);
         sidePanel.add(score);
         sidePanel.add(displayScore);
-        diceButton.addActionListener(this::actionPerformed);
-        sidePanel.setPreferredSize(new Dimension(266, 762));
+
+        // content pane
+        contentPane = new JPanel(new FlowLayout());
         contentPane.add(gamePanel);
         contentPane.add(sidePanel);
+
+        // main frame
+        mainFrame = new JFrame();
         mainFrame.setContentPane(contentPane);
         mainFrame.setSize(1028,762);
         mainFrame.pack();
